f641f58b5c66d20b5a3e7243fe39b2dc465456bf
RobertoPV <rprieto.valdivia@gmail.com>
2024-02-14 13:31:51 -0600

    Se declara matriz multidimensional

--- Codigos RDP/P02/p02.cpp
+++ Codigos RDP/P02/p02.cpp
@@ -1,5 +1,5 @@
 /*
-Programa 2. Uso de la estructura mat para crear matrices multidimensionales
+Programa 2. Uso de la estructura MAT para crear matrices multidimensionales
 */
 
 #include <iostream>
@@ -10,5 +10,13 @@ using namespace cv;
 
 int main()
 {
+	Mat matriz(3,3,CV_32FC2);
+	Mat m1 = Mat::ones(3,3,CV_32FC2);
+	//Unas propiedades de la clase Mat
+
+	cout << "Numero de filas: " << matriz.rows << endl;
+	cout << "Numero de columnas: " << matriz.cols << endl;
+	cout << "Tamanio de la matriz: " << matriz.size << endl;
+	cout << "Numero de canales: " << matriz.channels() << endl;
 	return 0;
 }
