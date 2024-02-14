f6b07960606fee839975554e1f66691247be90d9
RobertoPV <rprieto.valdivia@gmail.com>
2024-02-14 12:14:53 -0600

    Se relizaron pruebas a la matriz imagen y la mascara

--- Codigos PID/T1/jrImagenYMascara.cpp
+++ Codigos PID/T1/jrImagenYMascara.cpp
@@ -11,9 +11,43 @@ int main()
 {
 	cout << "Inicia programa donde se aplica una mascara a una imagen.\n";
 
-	Mat input;
+	Mat input, mask, res;
 	input = imread("C:/Users/rprie/OneDrive - Universidad de Guanajuato/MIE/Segundo Cuatrimestre/Procesamiento de imagenes/Imagenes/Tarea 1/Paisaje.jpg",IMREAD_COLOR);
 	imshow("Input",input);
+
+	mask = Mat::zeros(input.rows, input.cols, CV_8UC3);
+
+	for(int i = 0; i < 30; i++)
+	{
+		for(int j = 0; j < 30; j++)
+		{
+			mask.at<unsigned char>(i,j) = 255;
+		}
+	}
+	imshow("Mascara", mask);
+	
+	res.create(input.rows,input.cols,CV_8UC3);
+	/*for(int i = 0; i < input.rows; i++)
+	{
+		for(int j = 0; j < input.cols; j++)
+		{
+			if(mask.at<unsigned char>(i,j) == 255)
+				res.at<unsigned char>(i,j) = input.at<unsigned char>(i,j);
+		}
+	}*/
+	transform(input,res,mask);
+
+	//imshow("Resultado", res);
+
+	cout << input.rows << "\n";
+	cout << input.cols << "\n";
+	cout << input.channels() << "\n";
+	cout << mask.rows << "\n";
+	cout << mask.cols << "\n";
+	cout << mask.channels() << "\n";
+	cout << res.rows << "\n";
+	cout << res.cols << "\n";
+	cout << res.channels() << "\n";
 
 	waitKey(0);
 	return 0;
