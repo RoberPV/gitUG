63ecfa920945c6dca17fc73647389ee799272abf
RobertoPV <rprieto.valdivia@gmail.com>
2024-02-15 12:02:29 -0600

    Se aplica correctamente la mascara y se logra mover la mascara a lo largo de la imagen

--- Codigos PID/T1/jrImagenYMascara.cpp
+++ Codigos PID/T1/jrImagenYMascara.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 #include "opencv2\core.hpp"
 #include "opencv2\imgcodecs.hpp"
 #include "opencv2\highgui.hpp"
@@ -7,48 +8,94 @@
 using namespace std;
 using namespace cv;
 
+void resultadoConMascara(Mat r, Mat m, Mat inp);
+void generacionDeMascara(Mat m, int x, int y, int t);
+void movimientoMascara(int &x, int &y, Mat m);
+int t = 50;
+bool ys = true, xs = true;
+
 int main()
 {
 	cout << "Inicia programa donde se aplica una mascara a una imagen.\n";
 
 	Mat input, mask, res;
 	input = imread("C:/Users/rprie/OneDrive - Universidad de Guanajuato/MIE/Segundo Cuatrimestre/Procesamiento de imagenes/Imagenes/Tarea 1/Paisaje.jpg",IMREAD_COLOR);
-	imshow("Input",input);
+	int xpos = 0, ypos = 0;
+	mask = Mat::zeros(input.rows, input.cols, CV_8UC1);
+	res = Mat::zeros(input.rows, input.cols, CV_8UC3);
 
-	mask = Mat::zeros(input.rows, input.cols, CV_8UC3);
-
-	for(int i = 0; i < 30; i++)
+	do
 	{
-		for(int j = 0; j < 30; j++)
-		{
-			mask.at<unsigned char>(i,j) = 255;
-		}
+		generacionDeMascara(mask, xpos, ypos, t);
+		resultadoConMascara(res,mask,input);
+		
+		movimientoMascara(xpos, ypos, mask);
+
+		imshow("Resultado", res);
+		usleep(2000);
+		waitKey(10);
+		int key = waitKey(10);
+        if (key == 'q' || key == 27)
+            break;
 	}
-	imshow("Mascara", mask);
-	
-	res.create(input.rows,input.cols,CV_8UC3);
-	/*for(int i = 0; i < input.rows; i++)
-	{
-		for(int j = 0; j < input.cols; j++)
-		{
-			if(mask.at<unsigned char>(i,j) == 255)
-				res.at<unsigned char>(i,j) = input.at<unsigned char>(i,j);
-		}
-	}*/
-	transform(input,res,mask);
-
-	//imshow("Resultado", res);
-
-	cout << input.rows << "\n";
-	cout << input.cols << "\n";
-	cout << input.channels() << "\n";
-	cout << mask.rows << "\n";
-	cout << mask.cols << "\n";
-	cout << mask.channels() << "\n";
-	cout << res.rows << "\n";
-	cout << res.cols << "\n";
-	cout << res.channels() << "\n";
-
+	while(true);
 	waitKey(0);
 	return 0;
+}
+
+void resultadoConMascara(Mat r, Mat m, Mat inp)
+{
+	r = Mat::zeros(r.size(),CV_8UC3);
+	for(int i = 0; i < inp.rows; i++)
+	{
+		for(int j = 0; j < inp.cols; j++)
+		{
+			if(m.at<unsigned char>(i,j) == 255)
+			{
+				r.at<Vec3b>(i,j) = inp.at<Vec3b>(i,j);
+			}
+		}
+	}
+}
+
+void generacionDeMascara(Mat m, int x, int y, int t)
+{
+	m = Mat::zeros(m.size(), CV_8UC1);
+	for(int i = x; i < t + x; i++)
+	{
+		for(int j = y; j < t + y; j++)
+		{
+			m.at<unsigned char>(i,j) = 255;
+		}
+	}
+}
+
+void movimientoMascara(int &x, int &y, Mat m)
+{
+	if(x == 0)
+	{
+		xs = true;
+	}
+
+	if(y == 0)
+	{
+		ys = true;
+	}
+
+	if(x == m.rows - t)
+	{
+		xs = false;
+	}
+
+	if(xs)
+	{
+		x += 2;
+	}
+	else
+	{
+		x-=2;
+	}
+
+	if(ys) y ++;
+
 }
